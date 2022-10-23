1059a1acd0df51e1fc4be252e3cf1fa90d1bca93
Roman Amici <amicir@gmail.com>
2022-10-23 09:30:24 -0600

    A little more optimization

--- CodeGeneration/ASMUtils.cs
+++ CodeGeneration/ASMUtils.cs
@@ -1,7 +1,12 @@
+using System.Text.RegularExpressions;
+
 namespace Simp.CodeGeneration
 {
     public partial class ASMGenerator
     {
+        static Regex PushNumber = new Regex(@"push (\-)?[0-9]+", RegexOptions.Compiled);
+        static Regex Number = new Regex(@"(\-)?[0-9]+", RegexOptions.Compiled);
+
         void Clear(string reg)
         {
             Add($"xor {reg},{reg}");
@@ -27,7 +32,16 @@ namespace Simp.CodeGeneration
 
         void Pop(string register)
         {
-            if (FunctionText[^1] == $"push {register}")
+            // Replace push+pop of literal with mov
+            if (PushNumber.Match(FunctionText[^1]).Success)
+            {
+                var number = Number.Match(FunctionText[^1]).Value;
+                FunctionText.RemoveAt(FunctionText.Count - 1);
+                Add($"mov {register}, {number}");
+
+            }
+            // Replace push+pop of register entirely
+            else if (FunctionText[^1] == $"push {register}")
             {
                 FunctionText.RemoveAt(FunctionText.Count - 1);
             }
